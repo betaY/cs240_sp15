9c097874c913164a23fa01b993e30267267339c1
betaY <beta168921@gmail.com>
2015-01-26 08:51:50 -0500

    cs240 spring2015 lab2-b

---
+++ lab2b-src/mystring.c
@@ -0,0 +1,35 @@
+
+#include <stdlib.h>
+#include "mystring.h"
+
+// Type "man string" to see what every function expects.
+
+int mystrlen(char * s) {
+	return 0;
+}
+
+char * mystrcpy(char * dest, char * src) {
+	return NULL;
+}
+
+char * mystrcat(char * dest, char * src) {
+	return NULL;
+}
+
+int mystrcmp(char * s1, char * s2) {
+	return -1;
+}
+
+char * mystrstr(char * hay, char * needle) {
+	return NULL;
+}
+
+char * mystrdup(char * s) {
+	return NULL;
+}
+
+char * mymemcpy(char * dest, char * src, int n)
+{
+	return NULL;
+}
+
