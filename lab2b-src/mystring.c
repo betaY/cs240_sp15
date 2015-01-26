f223ee65b3697bc9c9037b7538a00dc31b0798d2
betaY <beta168921@gmail.com>
2015-01-26 14:13:26 -0500

    cs240 spring2015 lab2-b

--- lab2b-src/mystring.c
+++ lab2b-src/mystring.c
@@ -5,15 +5,28 @@
 // Type "man string" to see what every function expects.
 
 int mystrlen(char * s) {
-	return 0;
+    int count = 0;
+
+    while(*s++ != '\0')
+        count++;
+	return count;
 }
 
 char * mystrcpy(char * dest, char * src) {
-	return NULL;
+    char *temp = dest;
+
+    while ((*dest++ = *src++) != '\0');
+    
+
+	return temp;
 }
 
 char * mystrcat(char * dest, char * src) {
-	return NULL;
+    char *temp = dest;
+    while(*dest++ != '\0');
+    while((*dest++ = *src++) != '\0');
+    *dest = '\0';
+	return temp;
 }
 
 int mystrcmp(char * s1, char * s2) {
